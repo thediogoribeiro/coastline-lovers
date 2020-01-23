c56240f763c12273d5d0f6cc1796dd4d92dbf58e
Diogo Ribeiro <josediogoccbr@gmail.com>
2020-01-23 20:01:03 +0000

    add all html barebones, sass files and integration

---
+++ public/javascripts/script.js
@@ -0,0 +1,11 @@
+// MENU hamburger
+function showBurger() {
+  var x = document.getElementById("menu__links");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+
