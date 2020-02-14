adbe94e9a81da4b27bc272616ef22de035a778ae
Diogo Ribeiro <josediogoccbr@gmail.com>
2020-02-14 12:42:46 +0000

    fix merge conflicts

--- server.js
+++ server.js
@@ -28,10 +28,10 @@ app.use(express.static('public'));
 app.use(express.json({limit:'1mb'}));
 
 var BD = mysql.createConnection({
-	host : 'localhost',
-	user : 'root',
+    host : 'localhost',
+    port : 8888,
+    user : 'root',
     password : 'root',
-    port : '8889',
     database : 'coastlinelovers'
 });
 
@@ -42,13 +42,14 @@ BD.connect(function(err) {
         return;
     }
     console.log('Connected to database without errors');
+});
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/public/views/index.html'));
 });
 
 app.listen(port, host, function() {//poe o sv a correr
-	console.log('Running at: ',host,port);
+    console.log('Running at: ',host,port);
 });
 
 app.get('/Administrador', function(request, response){
