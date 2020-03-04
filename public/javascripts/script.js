4f417b93539c389588c4382dd5ec0040b56e462d
Diogo Ribeiro <josediogoccbr@gmail.com>
2020-03-04 12:32:02 +0000

    improve language dropdown

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -27,15 +27,24 @@ function openBooking(txt) {
 // Hamburger color change
 window.onscroll = function () {
   //console.log(this.scrollY);
-  const nav = document.getElementById('icon__effect');
-  if(this.scrollY <= 800){
+  var nav = document.getElementById('icon__effect');
+  var viewport = document.getElementById('about');
+  var x;
+  //console.log(viewport.getBoundingClientRect().top)
+  if (nav.className == "fas fa-bars fa-2x"){
+    x = 88
+  } else if (nav.className == "fas fa-bars fa-3x") {
+    x = 100
+  }
+
+  if(viewport.getBoundingClientRect().top > x){
     //console.log('branco');
     nav.style.color = '#FFFFFF';
     nav.style.borderColor = '#FFFFFF'; 
-  }else{
+  } else {
     //console.log('azul');
     nav.style.color = '#0E3356';
     nav.style.borderColor = '#0E3356';
-  }
+  };
 }
 
